38c491f15e1a912bddeedbe35a72b518f1120ffb
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-01-21 23:58:46 -0500

    se configura el toaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +12,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'TQ_TaskManager_Front_Angular';
+
+  constructor(private toastr: ToastrService){}
+  showToaster(){
+    this.toastr.success("Mensaje enviado con éxito");
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    })
+  ]
 };
