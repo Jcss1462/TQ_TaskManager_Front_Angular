e55f0a61aaa5873a0d59e8a1d772d28a42e50920
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-01-22 00:35:01 -0500

    se crean los componentes de usuario y el componente header que contendra la aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './domains/shared/components/header/header.component';
+import { LoginComponent } from './domains/user/login/login.component';
+import { ForgotPasswordComponent } from './domains/user/forgot-password/forgot-password.component';
+import { RegisterComponent } from './domains/user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        component: HeaderComponent,
+        children: [
+            {
+                path: "signin",
+                component: RegisterComponent
+            }
+            ,{
+                path: "login",
+                component: LoginComponent
+            }
+            ,{
+                path: "forgotPassword",
+                component: ForgotPasswordComponent
+            }
+       
+        ]
+    }
+
+
+];
